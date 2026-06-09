43ee510a250dfee54c0bed56ebcb8ca0922687b4
Mehedi <mehedi.interface@gmail.com>
2026-06-09 08:50:04 +0600

    Division Calculator

---
+++ example-3.js
@@ -0,0 +1,14 @@
+// Division Calculator 
+function divide (a, b) {
+    if (b === 0) {
+        throw new Error("Cannot divide by zero"); 
+    }
+
+    return a / b; 
+}
+
+try {
+    console.log(divide(10, 0)); 
+} catch (error) {
+    console.log(error.message);
+}
